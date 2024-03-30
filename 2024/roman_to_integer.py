5e02704881102eb071271ceeef3cd2fa796a8e89
DaJaFu <dnlfssll@gmail.com>
2024-03-30 16:18:54 -0400

    Create roman_to_integer.py
    Given a roman numeral, convert it to an integer.

---
+++ 2024/roman_to_integer.py
@@ -0,0 +1,12 @@
+def romanToInt(s: str) -> int:
+        num = 0
+        d = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        for i in range(len(s)):
+            if i == len(s)-1:
+                num += d.get(s[i])
+            elif d.get(s[i]) < d.get(s[i+1]):
+                num -= d.get(s[i])
+            else:
+                num += d.get(s[i])
+        return num
+print(romanToInt('VII'))
