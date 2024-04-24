9f376827df67450b57c5b6a16b461e76a860e956
DaJaFu <dnlfssll@gmail.com>
2024-04-23 20:14:46 -0400

    Finished Sudoku Solver
    Used a backtracing algorithm to find solution to sudoku solver.

--- 2024/sudoku_solver.py
+++ 2024/sudoku_solver.py
@@ -25,6 +25,15 @@ element is assigned a list of numbers, the program will iterate through again an
 if there are any elements with a single potential value, assign that value to the element.
 The thinking is that as this happens and the potential values go down for other
 elements, the program will steadily approach a final solution.
+
+Final Solution:
+After looking into backtracing algorithms, I implemented one using a nested 'for' loop
+and function recursion. The program will try using a value out of the possible values
+for that cell, then it will call the function again to see if there is a valid solution
+past that. If there is, it will return True, the value will be saved, and it will move to
+the next cell. If it returns false due to a solution not existing, it will reset the 
+cell and try again until it gets the right value. This is repeated for every empty cell
+until a solution is reached.
 '''
 
 from typing import List
@@ -55,17 +64,17 @@ def possible(board, row, col, num):
     return True
 
 def checkSolved(board: List[list]) -> bool:
-    def get_block(row:int,col:int)->list:
+    def get_block(row:int,col:int)->List[str]:
         x, y = 3*(row//3),3*(col//3)
         block = []
-        for i in range(x, y + 3):
-            block.extend(board[i][y:y+3])
+        for z in board[y:y+3]:
+            block.extend(z[x:x+3])
         return block
     
     def checkSection(section:list)->bool:
         seen = set()
         for cell in section:
-            if cell != '.' and type(cell) == str:
+            if cell != '.':
                 if cell in seen:
                     return False
                 seen.add(cell)
@@ -78,30 +87,28 @@ def checkSolved(board: List[list]) -> bool:
         if not (checkSection(row) and checkSection(col)):
             return False
     
-    for i in range(0, 9, 3):
-        for j in range(0, 9, 3):
-            block = get_block(i, j)
+    for row in range(3):
+        for col in range(3):
+            block = get_block(row*3,col*3)
             if not checkSection(block):
+                print(block)
                 return False
     return True
 
 def solveSudoku(board: List[List[str]]) -> None:
-        for row in range(9):
-            for col in range(9):
-                if board[row][col] == '.':
-                    lst = []
-                    for num in range(1,10):
-                        if possible(board,row,col,num):
-                            lst.append(str(num))
-                    if len(lst)>1:
-                        board[row][col]=lst
-                    else: 
-                        board[row][col]=lst[0]
-        if not checkSolved(board):
-            solveSudoku(board)
-            
-                    
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == '.':
+                for num in range(1,10):
+                    if possible(board,row,col,num):
+                        board[row][col] = str(num)
+                        if solveSudoku(board):
+                            return True    
+                        board[row][col] = '.'
+                return False
+    return True
+
 if __name__ == '__main__':
     solveSudoku(board)
-    print(board)
-    #print(f'\n Solved={checkSolved(board)}')
+    for row in board: print(f'\n {row}')
+    print(f'\n Solved: {checkSolved(board)}')
